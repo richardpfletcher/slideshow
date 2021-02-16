b8bd6c5d6cf0f66122b11911386b6f442b9c1755
richardpfletcher <richardpfletcher@gmail.com>
2021-02-16 16:27:08 -0600

    Add project files.

---
+++ whatscooking/Models/EmailModel.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+
+namespace heartoflove.Models
+{
+    
+
+        public class EmailModel
+        {
+            [Required]
+            [DataType(DataType.EmailAddress)]
+            [DisplayName("Email address")]
+            public string Email { get; set; }
+
+            [Required]
+            [DataType(DataType.Text)]
+            [DisplayName("Comments")]
+            public string Comments { get; set; }
+        }
+    
+}
